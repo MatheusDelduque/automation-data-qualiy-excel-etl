f91d0c8010b86b2a675d0733e2fe9d073ecf83bc
MatheusDelduque <matheusdelduque1@gmail.com>
2024-01-25 21:39:57 -0300

    Adicionado teste para checagem do titulo da pagina e para aberturaa do app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,34 @@
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from time import sleep
+import pytest
+import subprocess
 
-# Precismaos definir qual driver vamos utilizar
-driver = webdriver.Firefox()
+@pytest.fixture
+def driver():
+    # Iniciando o streamlit no background
+    process = subprocess.Popen(['streamlit', 'run', 'src/app.py'])
+    
+    # Iniciando o webdriver usando o GeckoDriver
+    driver = webdriver.Firefox()
+    driver.set_page_load_timeout(5)
+    yield driver
 
-# Define um timeout implícito
-driver.set_page_load_timeout(5)  # 5 segundos
-
-# Vamos fazer uma tratativa de try-except de entrar na nossa página
-try:
-    driver.get("http://localhost:8501")
-    sleep(5)
-    print("Acessou a página com sucesso")
-except TimeoutException:
-    print("Tempo de carregamento da página excedeu o limite.")
-finally:
+    # Encerrando o streamlit e o webdriver ao final do teste
+    process.terminate()
     driver.quit()
+
+def test_app_opens(driver):
+    # Verificando se o streamlit foi iniciado corretamente
+    driver.get('http://localhost:8501')
+
+def test_check_title_is(driver):
+    # Verificando se o streamlit foi iniciado corretamente
+    driver.get('http://localhost:8501')
+    sleep(5)
+
+    # Pegando o tiulo da página
+    page_title = driver.title
+
+    # Verificando se o tiulo da página e o esperado
+    expected_title = 'Validador de schemas de Excel'
+    assert page_title == expected_title, f"O titulo da pagina deve ser '{expected_title}', mas foi '{page_title}'"
