85dab92d44cca04b891933a546ed90f3a1a43444
MatheusDelduque <matheusdelduque1@gmail.com>
2024-01-26 15:26:11 -0300

    Adicionado testes para veriicar se h1 esta correto e se o arquivo e um excel correto

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 import pytest
 import subprocess
@@ -20,6 +22,7 @@ def driver():
 def test_app_opens(driver):
     # Verificando se o streamlit foi iniciado corretamente
     driver.get('http://localhost:8501')
+    driver.quit()
 
 def test_check_title_is(driver):
     # Verificando se o streamlit foi iniciado corretamente
@@ -32,3 +35,30 @@ def test_check_title_is(driver):
     # Verificando se o tiulo da página e o esperado
     expected_title = 'Validador de schemas de Excel'
     assert page_title == expected_title, f"O titulo da pagina deve ser '{expected_title}', mas foi '{page_title}'"
+    driver.close()
+
+def test_check_app_h1_is(driver):
+    # Verificando se o streamlit foi iniciado corretamente
+    driver.get('http://localhost:8501')
+    sleep(5)
+
+    # Pegando o h1 da página
+    page_h1 = driver.find_element(By.TAG_NAME, "h1").text
+
+    # Verificando se o h1 da página e o esperado
+    expected_h1 = 'Insira o arquivo Excel para validação'
+    assert page_h1 == expected_h1, f"O h1 da pagina deve ser '{expected_h1}', mas foi '{page_h1}'"
+    driver.close()
+
+def test_check_user_can_enter_excel(driver):
+    # Verificando se o streamlit foi iniciado corretamente
+    driver.get('http://localhost:8501')
+    sleep(5)
+
+    # Realizar o upload do arquivo Excel
+    success_file_path = os.path.abspath("data/success.xlsx")
+    driver.find_element(By.CSS_SELECTOR, "input[type='file']").send_keys(success_file_path)
+    sleep(5)
+
+    assert "O schema do arquivo Excel foi validado com sucesso!" in driver.page_source
+    driver.close()
