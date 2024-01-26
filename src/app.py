9fead5416e72c0035a0afb24c1088078dc4f771a
MatheusDelduque <matheusdelduque1@gmail.com>
2024-01-26 16:26:28 -0300

    Update

--- src/app.py
+++ src/app.py
@@ -1,32 +1,14 @@
 import streamlit as st
 
+# Titulo da página
+st.set_page_config(
+  page_title='Validador de schemas de Excel',
+)
+
 # Título do App
-st.title('Nosso Primeiro App com Streamlit')
+st.title('Insira o arquivo Excel para validação')
 
-# Escrevendo um Hello World com markdown
-st.markdown('**Hello world!** 🌍')
+arquivo = st.file_uploader('Carregue seu arquivo Excel', type=['xlsx'])
 
-# Escrevendo texto
-st.write('Esta é uma demonstração de algumas funcionalidades do Streamlit.')
-
-# Input de texto do usuário
-input_texto = st.text_input('Digite algo aqui:')
-
-# Mostrando o texto digitado
-st.write(f'Você digitou: {input_texto}')
-
-# Slider para números
-numero = st.slider('Escolha um número', 0, 100, 50)
-
-# Exibir o número escolhido
-st.write(f'Você escolheu o número: {numero}')
-
-# Gráfico de barras simples
-import pandas as pd
-import numpy as np
-
-# Criando dados aleatórios
-dados = pd.DataFrame({
-  'colunas': ['A', 'B', 'C', 'D', 'E'],
-  'valores': np.random.randn(5)
-})
+if arquivo:
+    st.success('O schema do arquivo Excel foi validado com sucesso!')
