e54fdf37b6cff4b1a6ac69d8bbcf83af3904289c
MatheusDelduque <matheusdelduque1@gmail.com>
2024-01-29 15:14:44 -0300

    Criado arquivo de contrato

---
+++ src/contract.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt, field_validator
+from datetime import datetime
+from enum import Enum
+
+class CategoryEnum(str, Enum):
+    food = "food"
+    entertainment = "entertainment"
+    health = "health"
+    education = "education"
+    others = "others"
+
+class Sales(BaseModel):
+    '''Modelo de dados para vendas
+        Argumentos:
+            email (str): Email do comprador
+            date (datetime): Data da compra
+            value (float): Valor da compra
+            amount (float): Quantidade de itens
+            product (str): Nome do item
+            category (str): Categoria da compra
+    '''
+
+    email: EmailStr
+    date: datetime
+    value: PositiveFloat
+    amount: PositiveFloat
+    product: str
+    category: CategoryEnum
+
+    # Validacao de categoria
+    @field_validator('category')
+    def validate_category(cls, error):
+        if error not in CategoryEnum:
+            raise ValueError('Invalid category')
+        return error
